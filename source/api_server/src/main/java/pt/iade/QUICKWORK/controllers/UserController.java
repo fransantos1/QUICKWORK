4fa9aa6b46e368154ec67dd96f8826ff8d9c15ab
fransantos1 <eusouofran@gmail.com>
2022-11-24 23:46:01 +0000

    added, add user and remove user to the api

--- source/api_server/src/main/java/pt/iade/QUICKWORK/controllers/UserController.java
+++ source/api_server/src/main/java/pt/iade/QUICKWORK/controllers/UserController.java
@@ -7,7 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,27 +21,36 @@ import pt.iade.QUICKWORK.models.repositories.UserRepository;
 @RequestMapping(path ="/api/users")
 public class UserController {
     private Logger Logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserRepository userRepository;
-    
-     //get all Users
+
+    // for debug porpuses, show all users
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<User> getUsers() {
-    Logger.info("Sending all units");
-    return userRepository.findAll();
+        Logger.info("Sending all units");
+        return userRepository.findAll();
     }
     //add user
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void addUser(@RequestBody User user){
-        Logger.info("deleting user");
-        
-    
 
-        
-    }
-    
-
+    public User saveUsr(@RequestBody User usr) {
+        Logger.info("User named: "+usr.getName()+" saved");
+        return userRepository.save(usr);
+    }   
     //remove a user 
+    @DeleteMapping(path="/{usrid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@PathVariable Integer usrid){
+        User user;
+        user = userRepository.findById(usrid).get();
+        String name = user.getName();
+
+        Logger.info("Delete user "+name);
+        userRepository.deleteById(usrid);
+    }
+
+
+
 
     
 }
